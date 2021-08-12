c0ae62666e9a813f11387c46f8a2e53b30f8376d
Sunil Kumar <sunilrajgir@gmail.com>
2021-08-12 12:32:23 +0530

    with throws

--- Throws.swift
+++ Throws.swift
@@ -1,20 +1,26 @@
-
-func test() {
-       var modifier: (Int) -> Int = { item in
-          return item*2
-      }
-      let items = [1,2,3,4].customMap { item in
-          return modifier(item)
-      }
-      print(items)
-      // 2,4,6,8
+enum CustomError: Error {
+    case customError
 }
+\
+       func memoryManagement() {
+        var modifier: (Int) throws -> Int = { item in
+            if Bool.random() {
+                return item*2
+            } else {
+                throw CustomError.customError
+            }
+        }
+        let items = try? [1,2,3,4].customMap { item in
+            return try modifier(item)
+        }
+        print(items)
+       }
 
 extension Array {
-    func customMap<T>(completion:((_ item:Element) -> T)) -> [T] {
+    func customMap<T>(completion:((_ item:Element) throws -> T)) throws -> [T] {
         var res = [T]()
         for item in self {
-            let modifiedItem = completion(item)
+            let modifiedItem = try completion(item)
             res.append(modifiedItem)
         }
         return res
